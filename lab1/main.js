e48f19df1f2071eb667bc9d0f9bd273c0b9282b5
unknown <vovan4oman1@gmail.com>
2025-11-27 20:27:37 +0200

    first commit

---
+++ lab1/main.js
@@ -0,0 +1,196 @@
+'use strict';
+
+let gl;                         // The webgl context.
+let surface;                    // A surface model
+let shProgram;                  // A shader program
+let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+
+function deg2rad(angle) {
+    return angle * Math.PI / 180;
+}
+
+
+// Constructor
+function Model(name) {
+    this.name = name;
+    this.iVertexBuffer = gl.createBuffer();
+    this.count = 0;
+
+    this.BufferData = function(vertices) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        this.count = vertices.length/3;
+    }
+
+    this.Draw = function() {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+   
+        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+    }
+}
+
+
+// Constructor
+function ShaderProgram(name, program) {
+
+    this.name = name;
+    this.prog = program;
+
+    this.iAttribVertex = -1;
+    this.iColor = -1;
+    this.iModelViewProjectionMatrix = -1;
+
+    this.Use = function() {
+        gl.useProgram(this.prog);
+    }
+}
+
+
+function draw() { 
+    gl.clearColor(0,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
+    
+    let modelView = spaceball.getViewMatrix();
+
+    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
+    let translateToPointZero = m4.translation(0,0,-10);
+
+    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
+    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
+        
+    
+    let modelViewProjection = m4.multiply(projection, matAccum1 );
+
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
+    
+    gl.uniform4fv(shProgram.iColor, [0,0.25,0.4,1] );
+
+    surface.Draw();
+}
+
+function CreateSurfaceData() {
+    let vertices = [];
+
+    let stepsU = 40;   
+    let stepsV = 40;
+
+    let twoPi = Math.PI * 2;
+    
+    function surf(u, v) {
+        // Формула Paul Bourke
+        const scale = 0.1;
+        let half = (0 <= u && u < Math.PI);
+
+        let r = 4 * (1 - Math.cos(u) / 2);
+
+        let x, y, z;
+
+        if (half) {
+            x = 6 * Math.cos(u) * (1 + Math.sin(u)) + r * Math.cos(u) * Math.cos(v);
+            y = 16 * Math.sin(u) + r * Math.sin(u) * Math.cos(v);
+        } else {
+            x = 6 * Math.cos(u) * (1 + Math.sin(u)) + r * Math.cos(v + Math.PI);
+            y = 16 * Math.sin(u);
+        }
+
+        z = r * Math.sin(v);
+        return [scale*x, scale*y, scale*z];
+    }
+
+    // Генеруємо сітку
+    for (let i = 0; i < stepsU; i++) {
+        let u0 = (i / stepsU) * twoPi;
+        let u1 = ((i + 1) / stepsU) * twoPi;
+
+        for (let j = 0; j <= stepsV; j++) {
+            let v = (j / stepsV) * twoPi;
+
+            let p1 = surf(u0, v);
+            let p2 = surf(u1, v);
+
+            // LINE_STRIP: додаємо по два
+            vertices.push(p1[0], p1[1], p1[2]);
+            vertices.push(p2[0], p2[1], p2[2]);
+        }
+    }
+
+    return vertices;
+}
+
+
+
+function initGL() {
+    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+
+    shProgram = new ShaderProgram('Basic', prog);
+    shProgram.Use();
+
+    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
+    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+
+    surface = new Model('Surface');
+    surface.BufferData(CreateSurfaceData());
+
+    gl.enable(gl.DEPTH_TEST);
+}
+
+
+function createProgram(gl, vShader, fShader) {
+    let vsh = gl.createShader( gl.VERTEX_SHADER );
+    gl.shaderSource(vsh,vShader);
+    gl.compileShader(vsh);
+    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+        throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
+     }
+    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    gl.shaderSource(fsh, fShader);
+    gl.compileShader(fsh);
+    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
+       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    }
+    let prog = gl.createProgram();
+    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, fsh);
+    gl.linkProgram(prog);
+    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
+       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    }
+    return prog;
+}
+
+
+function init() {
+    let canvas;
+    try {
+        canvas = document.getElementById("webglcanvas");
+        gl = canvas.getContext("webgl");
+        if ( ! gl ) {
+            throw "Browser does not support WebGL";
+        }
+    }
+    catch (e) {
+        document.getElementById("canvas-holder").innerHTML =
+            "<p>Sorry, could not get a WebGL graphics context.</p>";
+        return;
+    }
+    try {
+        initGL();  // initialize the WebGL graphics context
+    }
+    catch (e) {
+        document.getElementById("canvas-holder").innerHTML =
+            "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
+        return;
+    }
+
+    spaceball = new TrackballRotator(canvas, draw, 0);
+
+    draw();
+}
